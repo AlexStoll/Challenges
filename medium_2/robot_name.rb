4a79ee3afe67219d8b9e7996de7d0a374f50559d
Alex Stoll <alexstollm@gmail.com>
2019-07-18 14:54:19 -0700

    Refactored with the fun regexp-examples gem

--- medium_2/robot_name.rb
+++ medium_2/robot_name.rb
@@ -1,16 +1,15 @@
-
+require 'regexp-examples'
 
 class Robot
   attr_reader :name
   @@names_given = []
 
   def initialize
-    new_name = self.make_name
-    until !@@names_given.include?(new_name) do
-      new_name[4] = (0..9).select.to_s
+    @name = make_name
+    until !@@names_given.include?(@name)
+      @name = make_name
     end
-    @@names_given << new_name
-    @name = new_name
+    @@names_given << @name
   end
 
   def reset
@@ -20,16 +19,6 @@ class Robot
   protected
 
   def make_name
-    "#{rand_lett}#{rand_lett}#{rand_num}#{rand_num}#{rand_num}"
-  end
-
-  private
-
-  def rand_lett
-    ('A'..'Z').to_a.sample
-  end
-
-  def rand_num
-    (0..9).to_a.sample
+    /[A-Z]{2}[0-9]{3}/.random_example
   end
 end
